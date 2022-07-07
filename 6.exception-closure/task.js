3bb3c384ecde5762fa4813335fe1a470dd62c695
Андрей Богданов <cesaress@yandex.ru>
2022-07-07 19:15:00 +0300

    hw 6 complete

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,77 @@
+// Задача 2
+function parseCount(arg) {
+
+    let num = Number.parseInt(arg);
+
+    if (Number.isNaN(num)) {
+        throw new Error("Невалидное значение");
+    } else {
+        return num;
+    }
+
+}
+
+function validateCount(arg) {
+    
+    try {
+        return parseCount(arg);
+    } catch (e) {
+        return e;
+    }
+
+}
+
+
+
+// Задача 2
+class Triangle {
+
+    constructor (a, b, c) {
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if (((this.a + this.b) < this.c) 
+        || ((this.a + this.c) < this.b)
+        || ((this.b + this.c) < this.a)) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+
+    }
+
+    getPerimeter() {
+
+        return (this.a + this.b + this.c);
+
+    }
+
+    getArea() {
+
+        let p = this.getPerimeter() / 2;
+
+        return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+
+    }
+}
+
+function getTriangle(a, b, c) {
+
+    try {
+
+        return new Triangle(a, b, c);
+
+    } catch {
+
+        return {
+            getPerimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+            getArea() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+
+    }
+
+}
