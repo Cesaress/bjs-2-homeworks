9c3d6129b3d9e1f55db5cf3975599c7024702b25
Андрей Богданов <cesaress@yandex.ru>
2022-09-21 20:29:52 +0300

    hw 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,69 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cash = [];
+  let count = 0;
+
+  function wrapper(...args) {
+    const hash = args.join(",");
+
+    if (hash in cash) {
+      return "Из кэша: " + cash[hash];
+    }
+
+    if (count >= 5) {
+      delete cash[Object.keys(cash)[0]];
+    }
+
+    count++;
+
+    cash[hash] = func(...args);
+
+    return "Вычисляем: " + cash[hash];
+
+  }
+
+  return wrapper;
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let flag = false;
+  
+  return function(...args) {
+    
+    if (flag) {
+      return;
+    }
+
+    func.apply(this, args)
+    flag = true;
+    setTimeout(() => {
+      flag = false;
+      func.apply(this, args)
+    }, timeout);
+
+  }
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let flag = false;
+
+  function wrapper(...args) {
+
+    if (flag) {
+      return;
+    }
+
+    wrapper.count++;
+    func.apply(this,args);
+    flag = true;
+    setTimeout(() => {
+      flag = false;
+      func.apply(this, args);
+    }, timeout);
+
+  }
+
+  wrapper.count = 0;
+  
+  return wrapper;
 }
